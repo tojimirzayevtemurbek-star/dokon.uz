46440f6e7f32ca185657f24383587d47bcfdcab1
Temurbek <tojimirzayevtemurbek@gmail.com>
2026-09-25 08:56:01 +0500

    fix: allowed hosts added

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-*_02&rwp+by)t77j0od1=#y7dpu8i#-@n!ko%$afm1c5fnipj+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['dokon-uz.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
